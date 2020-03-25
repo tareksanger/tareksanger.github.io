ba595b5e91d5f47938995c873c8a4c50fe3e6fad
Tarek <tarek.sanger@me.com>
2020-03-24 21:08:25 -0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-## Web Portfolio
-This is my portfolio.
---
+++ precache-manifest.0acb1fe3da017fea19cb06aadf5de685.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8865fa9bcab8d31d74d04374e3c88ca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85fcd7588e1ec67f0a3a",
+    "url": "/static/css/main.7a70b513.chunk.css"
+  },
+  {
+    "revision": "bc98e1344b4e303d5105",
+    "url": "/static/js/2.ae6a2418.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/static/js/2.ae6a2418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85fcd7588e1ec67f0a3a",
+    "url": "/static/js/main.783474d1.chunk.js"
+  },
+  {
+    "revision": "19e2c7b65a68dbcf2593",
+    "url": "/static/js/runtime-main.b45daa86.js"
+  },
+  {
+    "revision": "3293616ec0c605c7c2db25829a0a509e",
+    "url": "/static/media/fontawesome-webfont.3293616e.woff"
+  },
+  {
+    "revision": "8b27bc96115c2d24350f0d09e6a9433f",
+    "url": "/static/media/fontawesome-webfont.8b27bc96.eot"
+  },
+  {
+    "revision": "b526f0637e912fae979bcfe9f0c9bd74",
+    "url": "/static/media/fontawesome-webfont.b526f063.svg"
+  },
+  {
+    "revision": "dcb26c7239d850266941e80370e207c1",
+    "url": "/static/media/fontawesome-webfont.dcb26c72.ttf"
+  },
+  {
+    "revision": "311a65fbb4995e88a76afc9c422e89dc",
+    "url": "/static/media/fontello.311a65fb.svg"
+  },
+  {
+    "revision": "3cd7423df7d808f922898f5be0edcdd8",
+    "url": "/static/media/fontello.3cd7423d.ttf"
+  },
+  {
+    "revision": "b54500d9503d30109f1a6acdb9fe81d8",
+    "url": "/static/media/fontello.b54500d9.woff"
+  },
+  {
+    "revision": "ce25cd5bdc97f3162b5ed8d9facb3005",
+    "url": "/static/media/fontello.ce25cd5b.eot"
+  },
+  {
+    "revision": "d4f483778a9516e9aff0daa6f9241b90",
+    "url": "/static/media/logo.d4f48377.svg"
+  }
+]);
